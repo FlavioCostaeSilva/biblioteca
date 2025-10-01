ba430379a48c7976059f8d8010b728d4df7c8953
FlavioCostaeSilva <flaviocosta@outlook.com>
2025-10-01 00:08:38 -0300

    Create homeController and autor controller

---
+++ app/app/Http/Controllers/AutorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Autor;
+use Illuminate\Http\Request;
+
+class AutorController extends Controller
+{
+    public function index()
+    {
+        $autores = Autor::all();
+        return view('autores.index', compact('autores'));
+    }
+}
